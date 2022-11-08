04eba44473c313421e2bba8094ae6cd9d7ad0281
Roberta Baraldi <roberta.baraldi@studio.unibo.it>
2022-11-08 02:56:50 +0100

    ctrl

--- GA_cond/functions.py
+++ GA_cond/functions.py
@@ -152,8 +152,8 @@ def ind_excel():
     ind_1 = pd.read_excel('Best_ind1.xlsx')
     ind_1 = ind_1.to_dict('index')
 
-    ind_2 = pd.read_excel('Best_ind2.xlsx')
-    ind_2 = ind_2.to_dict('index')
+    #ind_2 = pd.read_excel('Best_ind2.xlsx')
+    #ind_2 = ind_2.to_dict('index')
 
     ind_3 = pd.read_excel('Best_ind3.xlsx')
     ind_3 = ind_3.to_dict('index')
@@ -161,7 +161,10 @@ def ind_excel():
     ind_4 = pd.read_excel('Best_ind4.xlsx')
     ind_4 = ind_4.to_dict('index')
 
-    return ind_1, ind_2, ind_3, ind_4
+    ind_ctrl1 = pd.read_excel('Best_ind_ctrl1.xlsx')
+    ind_ctrl1 = ind_ctrl1.to_dict('index')
+
+    return ind_1, ind_3, ind_4, ind_ctrl1
 
 #%%
 def err_excel():
@@ -174,4 +177,6 @@ def err_excel():
 
     err_4 = pd.read_excel('Errors4.xlsx')
 
-    return err_1, err_2, err_3, err_4
+    err_ctrl1 = pd.read_excel('Errors_ctrl1.xlsx')
+
+    return err_1, err_2, err_3, err_4, err_ctrl1
--- GA_cond/plot_GA.py
+++ GA_cond/plot_GA.py
@@ -9,62 +9,48 @@ from math import log10
 from scipy.signal import find_peaks
 
 #%%
-###### PLOT BEST INDIVIDUALS FROM HCM_GA ALGORITHM #######
+###### PLOT HCM BEST IND #######
 
 t, v = baseline_run()
 plt.plot(t, v, '-k', label = 'Baseline')
 
-ind_1, ind_2, ind_3, ind_4 = ind_excel()
+ind_1, ind_3, ind_4, ind_ctrl1 = ind_excel()
 
-pop = [ind_1, ind_2, ind_3, ind_4]
+pop_HCM = [ind_1, ind_3, ind_4]
+pop_CTRL = [ind_ctrl1]
 
-for i in list(range(0,len(pop))):
-    t, v = plot_GA(pop[i])
-    plt.plot(t, v, label = f'Trial_{i+1}')
+for i in list(range(0,len(pop_HCM))):
+    t, v = plot_GA(pop_HCM[i])
+    plt.plot(t, v, label = f'Trial_HCM_{i+1}')
 
 plt.legend()
 plt.ylabel('Voltage (mV)', fontsize=14)
 plt.xlabel('Time (ms)', fontsize=14)
 plt.suptitle('Best Individuals: pop = 100, gen = 80', fontsize=14)
 plt.savefig('Plot_Best_Inds.png')
+
+####### PLOT CTRL BEST IND ########
+
+for i in list(range(0,len(pop_CTRL))):
+    t, v = plot_GA(pop_CTRL[i])
+    plt.plot(t, v, label = f'Trial_CTRL_{i+1}')
+
+plt.legend()
 plt.show()
 
-######## PRINT FEATURES ##########
-ap_features = {}
-mdp = min(v)
-max_p = max(v)
-max_p_idx = np.argmax(v)
-apa = max_p - mdp
-dvdt_max = np.max(np.diff(v[0:30])/np.diff(t[0:30]))
-
-ap_features['dvdt_max'] = dvdt_max
-peak_v = find_peaks(v, distance=100)
-peak = v[peak_v[0][0]]
-ap_features['peak'] = peak
-ap_features['apa']= apa
-
-for apd_pct in [50, 90]:
-    repol_pot = max_p - apa * apd_pct/100
-    idx_apd = np.argmin(np.abs(v[max_p_idx:] - repol_pot))
-    apd_val = t[idx_apd+max_p_idx]
-
-    ap_features[f'apd{apd_pct}'] = apd_val
-
-ap_features['mdp']= mdp
-
-#print(ap_features)
 
 #%%
-############ PLOT BEST ERROR ################
+############ PLOT BEST ERROR HCM ################
 gen = [i for i in list(range(1,80))]
 
-err_1, err_2, err_3, err_4 = err_excel()
+err_1, err_2, err_3, err_4, err_ctrl1 = err_excel()
 
-err = [err_1, err_2, err_3, err_4]
+err_HCM = [err_1, err_2, err_3, err_4]
+err_CTRL = [err_ctrl1]
 
-for i in list(range(0,len(err))):
-    best_err = list(err[i]['Avg Error'])
-    plt.plot(gen, best_err,'*', label = f'Trial_{i+1}')
+for i in list(range(0,len(err_HCM))):
+    best_err = list(err_HCM[i]['Best Error'])
+    plt.plot(gen, best_err,'*', label = f'Trial_HCM_{i+1}')
 
 plt.legend()
 plt.ylabel('Error', fontsize=14)
@@ -72,20 +58,24 @@ plt.xlabel('Generation', fontsize=14)
 plt.suptitle('Best Errors', fontsize=14)
 plt.ylim(0,5000)
 plt.savefig('Plot_Best_Errors.png')
+
+############ PLOT BEST ERROR CTRL ################
+
+for i in list(range(0,len(err_CTRL))):
+    best_err = list(err_CTRL[i]['Best Error'])
+    plt.plot(gen, best_err,'*', label = f'Trial_CTRL_{i+1}')
+
+plt.legend()
 plt.show()
 
 #%% 
-########## PLOT CONDUCTANCES ############
-trials = []
+########## PLOT HCM CONDUCTANCES ############
 
-for i in list(range(0,len(pop))):
-    trials.append(f'Trial_{i}')
-
-keys = [k for k in pop[0][0].keys()]
+keys = [k for k in pop_HCM[0][0].keys()]
 empty_arrs = [[] for i in range(len(keys))]
 all_ind_dict = dict(zip(keys, empty_arrs))
 
-for ind in pop:
+for ind in pop_HCM:
         for k, v in ind[0].items():
             all_ind_dict[k].append(v)
 
@@ -108,6 +98,42 @@ plt.hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
 plt.xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
 plt.ylim(log10(0.1), log10(10))
 plt.ylabel('Log10 Conductance', fontsize=14)
+plt.suptitle('Conductances_HCM')
 #plt.legend()
-plt.savefig('Conductances.png')
+plt.savefig('Conductances_HCM.png')
 plt.show()
+
+########## PLOT CTRL CONDUCTANCES ############
+
+keys = [k for k in pop_CTRL[0][0].keys()]
+empty_arrs = [[] for i in range(len(keys))]
+all_ind_dict = dict(zip(keys, empty_arrs))
+
+for ind in pop_CTRL:
+        for k, v in ind[0].items():
+            all_ind_dict[k].append(v)
+
+curr_x = 0
+n = 0
+
+for k, conds in all_ind_dict.items():
+    for i, g in enumerate(conds):
+        g = log10(g)
+        x = curr_x + np.random.normal(0, .01)
+        plt.scatter(x, g) #label = trials[n])
+
+    curr_x += 1
+
+    n += 1
+
+curr_x = 0
+
+plt.hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
+plt.xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
+plt.ylim(log10(0.1), log10(10))
+plt.ylabel('Log10 Conductance', fontsize=14)
+plt.suptitle('Conductances_CTRL')
+#plt.legend()
+plt.savefig('Conductances_CTRL.png')
+plt.show()
+
