8d32abcf9a3b671b849b4e982c818ee9700399fe
Roberta Baraldi <roberta.baraldi@studio.unibo.it>
2022-12-27 11:24:00 +0100

    newplot

--- GA_bNa_bCa_NaK/plot_GA.py
+++ GA_bNa_bCa_NaK/plot_GA.py
@@ -10,10 +10,10 @@ from scipy.signal import find_peaks
 
 #%%
 ###### PLOT HCM BEST IND ########
-plt.figure(figsize=(12,6))
+fig, axs = plt.subplots(1, 2, figsize=(20, 6))
 
 t, v = baseline_run()
-plt.plot(t, v, '-k', label = 'Baseline')
+axs[0].plot(t, v, '-k', label = 'Baseline')
 
 ind_1, ind_2, ind_3, ind_4, ind_5, ind_6, ind_7, ind_8, ind_9, ind_10, ind_ctrl1, ind_ctrl2, ind_ctrl3, ind_ctrl4, ind_ctrl5, ind_ctrl6 = ind_excel()
 
@@ -24,35 +24,68 @@ c_CTRL = ['lightgreen', 'lime', 'limegreen', 'yellowgreen', 'mediumseagreen', 'g
 
 for i in list(range(0,len(pop_HCM))):
     t, v = plot_GA(pop_HCM[i])
-    plt.plot(t, v, color=c_HCM[i], label = f'Trial_HCM_{i+1}')
+    axs[0].plot(t, v, color=c_HCM[i], label = f'Trial_HCM_{i+1}')
 
-plt.legend(loc='upper right')
-plt.ylabel('Voltage (mV)', fontsize=14)
-plt.xlabel('Time (ms)', fontsize=14)
-plt.suptitle('Best Individuals HCM: pop = 100, gen = 80', fontsize=14)
-plt.savefig('Plot_Best_Inds_HCM.png')
+axs[0].legend(loc='upper right')
+axs[0].set_ylabel('Voltage (mV)', fontsize=14)
+axs[0].set_xlabel('Time (ms)', fontsize=14)
+
+############ PLOT BEST ERROR HCM ################
+gen = [i for i in list(range(1,80))]
+
+err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10, err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6 = err_excel()
+
+err_HCM = [err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10]
+err_CTRL = [err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6]
+
+for i in list(range(0,len(err_HCM))):
+    best_err = list(err_HCM[i]['Best Error'])
+    axs[1].plot(gen, best_err,'*', color=c_HCM[i], label = f'Trial_HCM_{i+1}')
+
+axs[1].legend(loc='upper right')
+axs[1].set_ylabel('Error', fontsize=14)
+axs[1].set_xlabel('Generation', fontsize=14)
+axs[1].set_ylim(0,5000)
+
+fig.suptitle('HCM Individuals', fontsize=14)
+fig.savefig('Plot_HCM.png')
 plt.show()
 
-
+#%%
 ####### PLOT CTRL BEST IND ########
-plt.figure(figsize=(12,6))
+fig, axs = plt.subplots(1, 2, figsize=(20, 6))
 
 t, v = baseline_run()
-plt.plot(t, v, '-k', label = 'Baseline')
+axs[0].plot(t, v, '-k', label = 'Baseline')
 
 for i in list(range(0,len(pop_CTRL))):
     t, v = plot_GA(pop_CTRL[i])
-    plt.plot(t, v, color=c_CTRL[i], label = f'Trial_CTRL_{i+1}')
+    axs[0].plot(t, v, color=c_CTRL[i], label = f'Trial_CTRL_{i+1}')
 
-plt.legend(loc='upper right')
-plt.ylabel('Voltage (mV)', fontsize=14)
-plt.xlabel('Time (ms)', fontsize=14)
-plt.suptitle('Best Individuals CTRL: pop = 100, gen = 80', fontsize=14)
-plt.savefig('Plot_Best_Inds_CTRL.png')
+axs[0].legend(loc='upper right')
+axs[0].set_ylabel('Voltage (mV)', fontsize=14)
+axs[0].set_xlabel('Time (ms)', fontsize=14)
+
+############ PLOT BEST ERROR CTRL ################
+
+for i in list(range(0,len(err_CTRL))):
+    best_err = list(err_CTRL[i]['Best Error'])
+    axs[1].plot(gen, best_err,'*', color=c_CTRL[i], label = f'Trial_CTRL_{i+1}')
+
+axs[1].legend(loc='upper right')
+axs[1].set_ylabel('Error', fontsize=14)
+axs[1].set_xlabel('Generation', fontsize=14)
+axs[1].set_ylim(0,5000)
+
+fig.suptitle('CTRL Individuals', fontsize=14)
+fig.savefig('Plot_CTRL.png')
 plt.show()
 
+
+#%%
+
 ######## PLOT TOGETHER HCM AND CTRL ############
-plt.figure(figsize=(12,6))
+plt.figure(figsize=(14,8))
 
 t, v = baseline_run()
 plt.plot(t, v, '-k', label = 'Baseline')
@@ -68,52 +101,13 @@ for i in list(range(0,len(pop_CTRL))):
 plt.legend(loc='upper right')
 plt.ylabel('Voltage (mV)', fontsize=14)
 plt.xlabel('Time (ms)', fontsize=14)
-plt.suptitle('Best Individuals: pop = 100, gen = 80', fontsize=14)
-plt.savefig('Plot_Best_Inds.png')
+plt.savefig('Plot_Best_HCMandCTRL.png')
 plt.show()
-
-
-#%%
-############ PLOT BEST ERROR HCM ################
-plt.figure(figsize=(12,6))
-gen = [i for i in list(range(1,80))]
-
-err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10, err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6 = err_excel()
-
-err_HCM = [err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10]
-err_CTRL = [err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6]
-
-for i in list(range(0,len(err_HCM))):
-    best_err = list(err_HCM[i]['Best Error'])
-    plt.plot(gen, best_err,'*', color=c_HCM[i], label = f'Trial_HCM_{i+1}')
-
-plt.legend(loc='upper right')
-plt.ylabel('Error', fontsize=14)
-plt.xlabel('Generation', fontsize=14)
-plt.suptitle('Best Errors HCM', fontsize=14)
-plt.ylim(0,5000)
-plt.savefig('Plot_Best_Errors_HCM.png')
-plt.show()
-
-############ PLOT BEST ERROR CTRL ################
-plt.figure(figsize=(12,6))
-
-for i in list(range(0,len(err_CTRL))):
-    best_err = list(err_CTRL[i]['Best Error'])
-    plt.plot(gen, best_err,'*', color=c_CTRL[i], label = f'Trial_CTRL_{i+1}')
-
-plt.legend(loc='upper right')
-plt.ylabel('Error', fontsize=14)
-plt.xlabel('Generation', fontsize=14)
-plt.suptitle('Best Errors CTRL', fontsize=14)
-plt.ylim(0,5000)
-plt.savefig('Plot_Best_Errors_CTRL.png')
-plt.show()
-
 
 #%% 
+
 ########## PLOT HCM CONDUCTANCES ############
-plt.figure(figsize=(12,6))
+fig, axs = plt.subplots(1, 2, figsize=(20, 6))
 trials = []
 
 for i in list(range(0,len(pop_HCM))):
@@ -135,27 +129,23 @@ for k, conds in all_ind_dict.items():
         g = log10(g)
         x = curr_x + np.random.normal(0, .01)
         if m == 0:
-            plt.scatter(x, g, color=c_HCM[i], label = trials[i])
-            plt.legend()
+            axs[0].scatter(x, g, color=c_HCM[i], label = trials[i])
+            axs[0].legend(fontsize=5, loc='upper right')
         else:
-            plt.scatter(x, g, color=c_HCM[i])
+            axs[0].scatter(x, g, color=c_HCM[i])
     m = 1
 
     curr_x += 1
 
 curr_x = 0
 
-plt.hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
-plt.xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
-plt.ylim(log10(0.1), log10(10))
-plt.ylabel('Log10 Conductance', fontsize=14)
-plt.suptitle('Conductances_HCM')
-plt.savefig('Conductances_HCM.png')
-plt.show()
+axs[0].hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
+axs[0].set_xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
+axs[0].set_ylim(log10(0.1), log10(10))
+axs[0].set_ylabel('Log10 Conductance', fontsize=14)
 
 ########## PLOT CTRL CONDUCTANCES ############
 
-plt.figure(figsize=(12,6))
 trials = []
 
 for i in list(range(0,len(pop_CTRL))):
@@ -177,27 +167,30 @@ for k, conds in all_ind_dict.items():
         g = log10(g)
         x = curr_x + np.random.normal(0, .01)
         if m == 0:
-            plt.scatter(x, g, color=c_CTRL[i], label = trials[i])
-            plt.legend()
+            axs[1].scatter(x, g, color=c_CTRL[i], label = trials[i])
+            axs[1].legend(fontsize=5, loc='upper right')
         else:
-            plt.scatter(x, g, color=c_CTRL[i])
+            axs[1].scatter(x, g, color=c_CTRL[i])
     m = 1
 
     curr_x += 1
 
 curr_x = 0
 
-plt.hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
-plt.xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
-plt.ylim(log10(0.1), log10(10))
-plt.ylabel('Log10 Conductance', fontsize=14)
-plt.suptitle('Conductances_CTRL')
-plt.savefig('Conductances_CTRL.png')
+axs[1].hlines(0, -.5, (len(keys)-.5), colors='grey', linestyle='--')
+axs[1].set_xticks([i for i in range(0, len(keys))], ['GKs', 'GCaL', 'GKr', 'GNa', 'Gto', 'GK1', 'Gf','Gleak','GbNa','GbCa','GNaK'], fontsize=10)
+axs[1].set_ylim(log10(0.1), log10(10))
+axs[1].set_ylabel('Log10 Conductance', fontsize=14)
+
+fig.suptitle('Conductances HCM and CTRL')
+fig.savefig('Conductances.png')
 plt.show()
+
+#%%
 
 ######### PLOT CTRL AND HCM CONDUCTANCES TOGETHER ########
 
-plt.figure(figsize=(12,6))
+plt.figure(figsize=(14,8))
 trials = []
 
 for i in list(range(0,len(pop_HCM))):
@@ -220,7 +213,7 @@ for k, conds in all_ind_dict.items():
         x = curr_x + np.random.normal(0, .01)
         if m == 0:
             plt.scatter(x, g, color=c_HCM[i], label = trials[i])
-            plt.legend()
+            plt.legend(fontsize=5, loc='upper right')
         else:
             plt.scatter(x, g, color=c_HCM[i])
     m = 1
@@ -256,7 +249,7 @@ for k, conds in all_ind_dict.items():
         x = curr_x + np.random.normal(0, .01)
         if m == 0:
             plt.scatter(x, g, color=c_CTRL[i], label = trials[i])
-            plt.legend()
+            plt.legend(fontsize=5, loc='upper right')
         else:
             plt.scatter(x, g, color=c_CTRL[i])
     m = 1
@@ -268,7 +261,6 @@ curr_x = 0
 plt.suptitle('Conductances Comparison')
 plt.savefig('Conductances Comparison.png')
 plt.show()
-
 
 
 # %%
