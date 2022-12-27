731705143134747287f0e6715e4a320daee959da
Roberta Baraldi <roberta.baraldi@studio.unibo.it>
2022-12-27 11:06:05 +0100

    new

--- GA_bNa_bCa_NaK/stim.py
+++ GA_bNa_bCa_NaK/stim.py
@@ -8,9 +8,9 @@ from scipy.signal import find_peaks
 
 #%%
 ############## STIMULATED HCM POPULATION ##############
-plt.figure(figsize=(14,8))
+fig, axs = plt.subplots(1, 2, figsize=(28, 6))
 t, v = baseline_run()
-plt.plot(t, v, '-k', label = 'Baseline')
+axs[0].plot(t, v, '-k', label = 'Baseline')
 
 ind_1, ind_2, ind_3, ind_4, ind_5, ind_6, ind_7, ind_8, ind_9, ind_10, ind_ctrl1, ind_ctrl2, ind_ctrl3, ind_ctrl4, ind_ctrl5, ind_ctrl6 = ind_excel()
 
@@ -20,33 +20,29 @@ c_CTRL = ['lightgreen', 'lime', 'limegreen', 'yellowgreen', 'mediumseagreen', 'g
 
 for i in list(range(0,len(pop_HCM))):
     t_leak, v_leak, t_rrc, v_rrc = stim(pop_HCM[i])
-    plt.plot(t_leak, v_leak, color=c_HCM[i], label = f'Trial_HCM_{i+1}')
-    plt.plot(t_rrc, v_rrc, color=c_CTRL[i], label = f'Trial_Stim_HCM_{i+1}')
+    axs[0].plot(t_leak, v_leak, color=c_HCM[i], label = f'Trial_HCM_{i+1}')
+    axs[0].plot(t_rrc, v_rrc, color=c_CTRL[i], label = f'Trial_Stim_HCM_{i+1}')
 
-plt.legend(loc='upper right')
-plt.ylabel('Voltage (mV)', fontsize=14)
-plt.xlabel('Time (ms)', fontsize=14)
-plt.suptitle('Best HCM Individuals - Stimulus Injection', fontsize=14)
-plt.savefig('Plot_Stim_HCM.png')
-plt.show()
+axs[0].legend(fontsize= 5, loc='lower center')
+axs[0].set_ylabel('Voltage (mV)', fontsize=14)
+axs[0].set_xlabel('Time (ms)', fontsize=14)
 
 ############## STIMULATED CTRL POPULATION ##############
-plt.figure(figsize=(14,8))
 t, v = baseline_run()
-plt.plot(t, v, '-k', label = 'Baseline')
+axs[1].plot(t, v, '-k', label = 'Baseline')
 
 pop_CTRL = [ind_ctrl1, ind_ctrl2, ind_ctrl3, ind_ctrl4, ind_ctrl5, ind_ctrl6]
 
 for i in list(range(0,len(pop_CTRL))):
     t_leak, v_leak, t_rrc, v_rrc = stim(pop_CTRL[i])
-    plt.plot(t_leak, v_leak, color=c_HCM[i], label = f'Trial_CTRL_{i+1}')
-    plt.plot(t_rrc, v_rrc, color=c_CTRL[i], label = f'Trial_Stim_CTRL_{i+1}')
+    axs[1].plot(t_leak, v_leak, color=c_HCM[i], label = f'Trial_CTRL_{i+1}')
+    axs[1].plot(t_rrc, v_rrc, color=c_CTRL[i], label = f'Trial_Stim_CTRL_{i+1}')
 
-plt.legend(loc='upper right')
-plt.ylabel('Voltage (mV)', fontsize=14)
-plt.xlabel('Time (ms)', fontsize=14)
-plt.suptitle('Best CTRL Individuals - Stimulus Injection', fontsize=14)
-plt.savefig('Plot_Stim_CTRL.png')
+axs[1].legend(fontsize= 5, loc='upper right')
+axs[1].set_ylabel('Voltage (mV)', fontsize=14)
+axs[1].set_xlabel('Time (ms)', fontsize=14)
+
+fig.savefig('Plot_CurrentInjection.png')
 plt.show()
 
 # %%
