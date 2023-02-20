128ac4beaec79c3bd4c201745c788793d445ae96
Roberta Baraldi <roberta.baraldi@studio.unibo.it>
2023-02-20 04:00:42 +0100

    new

--- plot_GA.py
+++ plot_GA.py
@@ -32,27 +32,6 @@ axs.set_xlabel('Time (ms)', fontsize=14)
 fig.savefig('HCM.png')
 plt.show()
 
-############ PLOT BEST ERROR HCM ################
-gen = [i for i in list(range(1,80))]
-fig, axs = plt.subplots(1, 1, figsize=(12, 8))
-
-err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10, err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6, err_ctrl7, err_ctrl8, err_ctrl9, err_ctrl10 = err_excel()
-
-err_HCM = [err_1, err_2, err_3, err_4, err_5, err_6, err_7, err_8, err_9, err_10]
-err_CTRL = [err_ctrl1, err_ctrl2, err_ctrl3, err_ctrl4, err_ctrl5, err_ctrl6, err_ctrl7, err_ctrl8, err_ctrl9, err_ctrl10]
-
-for i in list(range(0,len(err_HCM))):
-    best_err = list(err_HCM[i]['Best Error'])
-    axs.plot(gen, best_err,'*', color=c_HCM[i], label = f'Trial_HCM_{i+1}')
-
-axs.legend(loc='upper right')
-axs.set_ylabel('Error', fontsize=14)
-axs.set_xlabel('Generation', fontsize=14)
-axs.set_ylim(0,4000)
-
-fig.savefig('Errors_HCM.png')
-plt.show()
-
 #%%
 ####### PLOT CTRL BEST IND ########
 fig, axs = plt.subplots(1, 1, figsize=(12, 8))
@@ -69,22 +48,6 @@ axs.set_ylabel('Voltage (mV)', fontsize=14)
 axs.set_xlabel('Time (ms)', fontsize=14)
 fig.savefig('CTRL.png')
 plt.show()
-
-############ PLOT BEST ERROR CTRL ################
-fig, axs = plt.subplots(1, 1, figsize=(12, 8))
-
-for i in list(range(0,len(err_CTRL))):
-    best_err = list(err_CTRL[i]['Best Error'])
-    axs.plot(gen, best_err,'*', color=c_CTRL[i], label = f'Trial_CTRL_{i+1}')
-
-axs.legend(loc='upper right')
-axs.set_ylabel('Error', fontsize=14)
-axs.set_xlabel('Generation', fontsize=14)
-axs.set_ylim(0,4000)
-
-fig.savefig('Errors_CTRL.png')
-plt.show()
-
 
 #%%
 
