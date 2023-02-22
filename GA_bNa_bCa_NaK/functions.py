a5340574e47576d70a5a5ae316244c5431b7dffd
Roberta Baraldi <roberta.baraldi@studio.unibo.it>
2023-02-22 17:48:42 +0100

    new

--- GA_bNa_bCa_NaK/functions.py
+++ GA_bNa_bCa_NaK/functions.py
@@ -27,7 +27,7 @@ def plot_GA(ind):
 
     t = np.array(dat['engine.time'][start_ap:end_ap])
     t = t - t[0]
-    max_idx = np.argmin(np.abs(t-900))
+    max_idx = np.argmin(np.abs(t-980))
     t_leak = t[0:max_idx]
     end_ap = start_ap + max_idx
 
